aec63746a550fdbdf0d0553235ba0e969ced4563
Micah Chambers <micahc.vt@gmail.com>
2020-01-15 10:22:13 -0800

    added batchnorm, loss goes down

--- autodet/Encoder.py
+++ autodet/Encoder.py
@@ -6,8 +6,10 @@ class Encoder(nn.Module):
     def __init__(self, ichannels, ochannels):
         super(Encoder, self).__init__()
         self.conv1 = nn.Conv2d(ichannels, 20, 3)
+        self.conv1_bn = nn.BatchNorm2d(20)
         self.conv2 = nn.Conv2d(20, ochannels, 3)
+        self.conv2_bn = nn.BatchNorm2d(ochannels)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        return F.relu(self.conv2(x))
+        x = F.relu(self.conv1_bn(self.conv1(x)))
+        return F.relu(self.conv2_bn(self.conv2(x)))
--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import os
 
 import matplotlib.pyplot as plt
 import torch
+from torch.utils import data
 
 import autodet
+
 
 from autodet.UnlabeledDirectoryDataset import UnlabeledDirectoryDataset
 from autodet.Encoder import Encoder
@@ -16,16 +18,28 @@ from autodet.Loss import Loss
 def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('-i', '--idir', required=True,
-                        help='Input file directory')
+                        help='input file directory')
+    parser.add_argument('-e', '--epochs', default=1, type=int,
+                        help='input file directory')
+    parser.add_argument('-N', '--batch-size', default=1, type=int,
+                        help='Batch size')
 
     args = parser.parse_args()
+    dataset_params = {
+        'batch_size': args.batch_size,
+        'shuffle': True,
+        'num_workers': 6,
+    }
 
     if torch.cuda.is_available():
+        print("Using CUDA")
         device = torch.device('cuda')
     else:
+        print("Using CPU")
         device = torch.device('cpu')
 
-    dataset = UnlabeledDirectoryDataset(args.idir)
+    training_set = UnlabeledDirectoryDataset(args.idir)
+    training_gen = data.DataLoader(training_set, **dataset_params)
     encoder = Encoder(ichannels=3, ochannels=20)
     decoder = Decoder(ichannels=20, ochannels=3)
     losser = Loss()
@@ -33,7 +47,7 @@ def main():
     # construct an optimizer
     params = [p for p in encoder.parameters() if p.requires_grad] + \
         [p for p in decoder.parameters() if p.requires_grad]
-    optimizer = torch.optim.Adam(params, lr=0.01)
+    optimizer = torch.optim.Adam(params, lr=0.001)
     optimizer.zero_grad()
 
     encoder.to(device)
@@ -41,26 +55,25 @@ def main():
     losser.to(device)
 
     #fig = plt.figure()
-    for i in range(len(dataset)):
-        sample = dataset[i]
+    for epoch in range(args.epochs):
+        for sample in training_gen:
 
-        # "batch" by adding dim 0
-        img = torch.tensor([sample['image']],
-                           dtype=torch.float32, device=device)
+            # "batch" by adding dim 0
+            img = (sample['image'] / 255.0).to(device)
 
-        encoded = encoder(img)
-        decoded = decoder(encoded)
+            encoded = encoder(img)
+            decoded = decoder(encoded)
 
-        # loss
-        loss = losser(target=img, predicted=decoded)
-        loss.backward()
+            # loss
+            loss = losser(target=img, predicted=decoded)
+            loss.backward()
 
-        optimizer.step()
+            optimizer.step()
 
-        print(loss)
+            print(loss)
 
-        #plt.imshow(sample['image'])
-        #plt.show()
+            #plt.imshow(sample['image'])
+            #plt.show()
 
 
 if __name__ == '__main__':
