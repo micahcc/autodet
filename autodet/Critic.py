9fea777f50b16f46d1eea0e5bbd5a7e983f3300a
Micah Chambers <micahc.vt@gmail.com>
2020-01-17 09:51:57 -0800

    improve residual

--- autodet/Critic.py
+++ autodet/Critic.py
@@ -7,13 +7,10 @@ from autodet.Residual import Residual
 class Critic(nn.Module):
     def __init__(self, ichannels, ochannels):
         super(Critic, self).__init__()
-        self.layer0 = nn.Conv2d(ichannels, 20, 1)
-        self.layer1 = Residual(ichannels=20, ochannels=20, kradius=1)
-        self.layer2 = nn.Conv2d(20, ochannels, 1)
+        self.layer1 = Residual(ichannels=ichannels, ochannels=20, kradius=1)
+        self.layer2 = Residual(ichannels=20, ochannels=ochannels, kradius=1)
 
     def forward(self, x):
-        x = self.layer0(x)
-        x = F.softsign(x)
         x = self.layer1(x)
         x = F.softsign(x)
         x = self.layer2(x)
--- autodet/Residual.py
+++ autodet/Residual.py
@@ -3,17 +3,31 @@ import torch.nn.functional as F
 
 
 class Residual(nn.Module):
-    def __init__(self, ichannels, ochannels, kradius=1):
+    def __init__(self, ichannels, ochannels, kradius=1, project_activation=F.softsign):
         super(Residual, self).__init__()
         self.kradius = kradius
         self.kwidth = self.kradius * 2 + 1
 
-        assert ichannels == ochannels
-        self.conv = nn.Conv2d(ichannels, ochannels, self.kwidth)
+        if ichannels != ochannels:
+            self.conv_project = nn.Conv2d(ichannels, ochannels, 1)
+            self.conv_project_bn = nn.BatchNorm2d(ochannels)
+            self.project_activation = project_activation
+        else:
+            self.conv_project = None
+            self.conv_project_bn = None
+            self.project_activation = None
+
+        self.conv = nn.Conv2d(ochannels, ochannels, self.kwidth)
         self.conv_bn = nn.BatchNorm2d(ochannels)
 
     def forward(self, x):
-        # compute difference
+        # project into a dimension to match input and output channels
+        if self.conv_project and self.conv_project_bn and self.project_activation:
+            x = self.conv_project(x)
+            x = self.conv_project_bn(x)
+            x = self.project_activation(x)
+
+        # compute difference, then crop input down the convolved version
         dx = self.conv_bn(self.conv(x))
         cx = x[..., self.kradius: -self.kradius, self.kradius: -self.kradius]
         x = cx + dx
